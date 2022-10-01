435c01caa6223cd23484c380f5afb2a639e57ab8
marvin evins <marvinevins@yahoo.com>
2022-10-01 08:23:21 -0500

    Initial Commit

---
+++ pokemon/pokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemonApp.swift
+//  pokemon
+//
+//  Created by marvin evins on 10/1/22.
+//
+
+import SwiftUI
+
+@main
+struct pokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
