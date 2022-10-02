848cff508c8642500dc9fa2b9c1da988016d7007
marvin evins <marvinevins@yahoo.com>
2022-10-02 09:41:49 -0500

    networking code

---
+++ pokemon/ViewModel/PokemonViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  PokemonViewModel.swift
+//  pokemon
+//
+//  Created by marvin evins on 10/2/22.
+//
+
+import Foundation
+
+
+class PokemonViewModel: ObservableObject{
+    @Published var pokemon = [Pokemon]()
+    
+    let baseUrl = "https://pokedex-bb36f.firebaseio.com/pokemon.json"
+    
+    func fetchPokemon(){
+        guard let url = URL(string: baseUrl) else {return}
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            guard let data = data?.parseData(removeString: "null, ") else {return }
+            
+            guard let pokemon = try? JSONDecoder().decode([Pokemon].self, from : data) else{return }
+                    
+                    DispatchQueue.main.async{
+                        self.pokemon = pokemon
+                    }
+        }.resume()
+    }
+}
+
+extension Data{
+    func parseData(removeString string: String) -> Data?{
+        let dataAsString = String(data: self, encoding: .utf8)
+        let parsedDataString = dataAsString?.replacingOccurrences(of: string, with: "")
+        guard let data = parsedDataString?.data(using: .utf8) else {return nil}
+        return data
+    }
+}
